7b5fcc56f94fa6d3f3cf2e4b365ae7a4e1aff59b
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-11-16 02:59:19 +0400

    Patch: lcstore-accel Type: feature
    Add Localisation Cache support for CACHE_ACCEL. CustIS Bugs 75715, 68935

--- includes/LocalisationCache.php
+++ includes/LocalisationCache.php
@@ -154,8 +154,14 @@ class LocalisationCache {
 				case 'db':
 					$storeClass = 'LCStore_DB';
 					break;
+				case 'accel':
 				case 'detect':
-					$storeClass = $wgCacheDirectory ? 'LCStore_CDB' : 'LCStore_DB';
+					try {
+						wfGetCache( CACHE_ACCEL );
+						$storeClass = 'LCStore_Accel';
+					} catch( MWException $e ) {
+						$storeClass = $wgCacheDirectory ? 'LCStore_CDB' : 'LCStore_DB';
+					}
 					break;
 				default:
 					throw new MWException(
@@ -316,7 +322,10 @@ class LocalisationCache {
 		}
 
 		$deps = $this->store->get( $code, 'deps' );
-		if ( $deps === null ) {
+		$keys = $this->store->get( $code, 'list', 'messages' );
+		$preload = $this->store->get( $code, 'preload' );
+		// Different keys may expire separately, at least in LCStore_Accel
+		if ( !$deps || !$keys || !$preload ) {
 			wfDebug( __METHOD__."($code): cache missing, need to make one\n" );
 			return true;
 		}
@@ -744,6 +753,54 @@ interface LCStore {
 	 */
 	function set( $key, $value );
 
+}
+
+/**
+ * LCStore implementation which uses PHP accelerator to store data.
+ * This will work if one of XCache, eAccelerator, or APC cacher is configured.
+ * (See ObjectCache.php)
+ */
+class LCStore_Accel implements LCStore {
+	var $currentLang;
+	var $keys;
+
+	public function __construct() {
+		$this->cache = wfGetCache( CACHE_ACCEL );
+	}
+
+	public function get( $code, $key ) {
+		$k = wfMemcKey( 'l10n', $code, 'k', $key );
+		return $this->cache->get( $k );
+	}
+
+	public function startWrite( $code ) {
+		$k = wfMemcKey( 'l10n', $code, 'l' );
+		$keys = $this->cache->get( $k );
+		if ( $keys ) {
+			foreach ( $keys as $k ) {
+				$this->cache->delete( $k );
+			}
+		}
+		$this->currentLang = $code;
+		$this->keys = array();
+	}
+
+	public function finishWrite() {
+		if ( $this->currentLang ) {
+			$k = wfMemcKey( 'l10n', $this->currentLang, 'l' );
+			$this->cache->set( $k, array_keys( $this->keys ) );
+		}
+		$this->currentLang = NULL;
+		$this->keys = array();
+	}
+
+	public function set( $key, $value ) {
+		if ( $this->currentLang ) {
+			$k = wfMemcKey( 'l10n', $this->currentLang, 'k', $key );
+			$this->keys[$k] = true;
+			$this->cache->set( $k, $value );
+		}
+	}
 }
 
 /**
