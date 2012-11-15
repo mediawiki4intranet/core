5b6cbbdd59462d562ee1b8ffa16b651015dc7d6b
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-11-16 00:40:29 +0400

    Patch: anchor-link-tocnumbers Type: feature
    Add numbers to section links. CustIS Bug 54239

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -4658,6 +4658,11 @@ class Parser {
 
 			# Don't number the heading if it is the only one (looks silly)
 			if ( count( $matches[3] ) > 1 && $this->mOptions->getNumberHeadings() ) {
+				# CustIS Bug 54239 - Number [[#Section|Section]] links
+				if ( empty( $headNumberReplacer ) ) {
+					$headNumberReplacer = new ReplacementArray();
+				}
+				$headNumberReplacer->setPair('>'.$headlineHint.'</a>', '>'.$numbering.' '.$headlineHint.'</a>');
 				# the two are different if the line contains a link
 				$headline = Html::element(
 					'span',
@@ -4759,6 +4764,10 @@ class Parser {
 		if ( $isMain ) {
 			$this->mOutput->setSections( $tocraw );
 		}
+
+		# Bug 54239 - Number [[#Section|Section]] links
+		if ( !empty( $headNumberReplacer ) )
+			$text = $headNumberReplacer->replace($text);
 
 		# split up and insert constructed headlines
 		$blocks = preg_split( '/<H[1-6].*?>[\s\S]*?<\/H[1-6]>/i', $text );
