d57953d6c97f857695fa5347d47acb73bcb6549c
Vitaliy Filippov <vitalif@yourcmc.ru>
2013-07-04 17:30:25 +0400

    Patch: urldecode-printable-links Type: bugfix
    Display url-decoded source link on the printable version; remove
    link expansion for non-existing error message and more (class=plainlinks).

--- includes/skins/Skin.php
+++ includes/skins/Skin.php
@@ -639,7 +639,7 @@ abstract class Skin extends ContextSource {
 		}
 
 		return $this->msg( 'retrievedfrom' )
-			->rawParams( '<a dir="ltr" href="' . $url . '">' . $url . '</a>' )
+			->rawParams( '<a dir="ltr" href="' . $url . '">' . urldecode( $url ) . '</a>' )
 			->parse();
 	}
 
